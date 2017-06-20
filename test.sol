6b6369fde09bf2b2b00155f615718860ebcbc984
jjcool <jjjayesh26@gmail.com>
2017-06-20 13:41:00 +0530

    Counter added

--- test.sol
+++ test.sol
@@ -20,21 +20,39 @@ contract Conses{
 		string doc_url;
 		uint createdAt;
 		uint premium ; 
+		string status ; 
 	}
 	 mapping (address => uint[]) public customerPolicies;
+	 mapping (uint => uint) public counter ; 
+
 
 	function new_request(string reference_id, string carrier , uint premium) returns (bool) {
 
 		uint policyId = policies.length++;
         customerPolicies[this].push(policyId);
         policy p = policies[policyId];
-        
+
         p.reference_id = reference_id ; 
         p.carrier = carrier ; 
         p.premium = premium ; 
-
+        p.status = "To be approved " ;
+        counter[policyId] = 0 ;
+        
         return true ; 
 
+	}
+    // uint k  = 100 ;
+	function counter(uint _policyId) returns (string) {
+
+		
+		counter[_policyId] = counter[_policyId]+  1; 
+		policy x = policies[_policyId] ;
+
+		if (counter[_policyId]  > 3 ){
+			x.status = "approved";
+		} 
+
+		return x.status ;
 	}
 
 
